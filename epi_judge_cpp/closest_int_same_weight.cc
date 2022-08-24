fbd2dfc01d952ac647d023d4f478135f7fe7e8b7
adamchen3 <weiguan.chen@outlook.com>
2022-08-24 22:39:22 +0800

    update problem description

--- epi_judge_cpp/closest_int_same_weight.cc
+++ epi_judge_cpp/closest_int_same_weight.cc
@@ -6,7 +6,7 @@
 // Write a program which takes as input a nonnegative integer x and returns a number
 // y which is not equal to x, but has the same weight as x and their difference, |y - x|, is
 // as small as possible. You can assume x is not 0, or all 1s. For example, if x = 6(110), You
-// should return 5(011).
+// should return 5(101).
 // 
 // Hint: Start with the least significant bit.
 
